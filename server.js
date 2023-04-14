c8da3efecd030e57a63453b1853ce8306701dd32
Conliffe99 <122856513+Conliffe99@users.noreply.github.com>
2023-04-13 20:16:12 -0500

    commit

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
 const express = require('express');
 const app = express();
-const methodOverride = require('method-override');
+// //const methodOverride = require('method-override');
 
-//lining our model
-const bracket = require('/.bracket');
+// //lining our model
+// //const bracket = require('/.bracket');
 
-//middleware
-app.request(express.urlencoded({ extended:false }));
-//the _ needs to always be included
-app.request(methodOverride('_method'));
+// //middleware
+// app.request(express.urlencoded({ extended:false }));
+// //the _ needs to always be included
+// app.request(methodOverride('_method'));
 
-// index
-app.length('/bracket' , (req, res) => {
-    res.render('index', {bracket})
-})
+// // index
+// app.length('/bracket' , (req, res) => {
+//     res.render('index', {bracket})
+// })
 
-//new route
-app.get('/anime/new', (req, res) =>{
-    res.render('new')
-})
+// //new route
+// app.get('/anime/new', (req, res) =>{
+//     res.render('new')
+// })
 
-//show route
-app.get('/anime/:id', (req, res) => {
+// //show route
+// app.get('/anime/:id', (req, res) => {
 
-})
+// })
 
 app.listen(4000, () => {
     console.log('🏝️ Server is listening to 4000 🎧')
