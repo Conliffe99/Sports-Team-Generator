7b493b26eb1404da303eb6c95adf14e99aa5569f
Conliffe99 <122856513+Conliffe99@users.noreply.github.com>
2023-04-15 12:23:14 -0500

    commit

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+// Initialize the app
 const app = express();
-// //const methodOverride = require('method-override');
 
-// //lining our model
-// //const bracket = require('/.bracket');
+// Connect to the database
+mongoose.connect('mongodb://localhost:27017/sports-bracket', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+}).then(() => {
+  console.log('Connected to the database!');
+}).catch((err) => {
+  console.error(err);
+});
 
-// //middleware
-// app.request(express.urlencoded({ extended:false }));
-// //the _ needs to always be included
-// app.request(methodOverride('_method'));
+// Set up middleware
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
-// // index
-// app.length('/bracket' , (req, res) => {
-//     res.render('index', {bracket})
-// })
+// Set up routes
+const teamRoutes = require('./routes/teams');
+app.use('/teams', teamRoutes);
 
-// //new route
-// app.get('/anime/new', (req, res) =>{
-//     res.render('new')
-// })
-
-// //show route
-// app.get('/anime/:id', (req, res) => {
-
-// })
-
+// Start the server
 app.listen(4000, () => {
-    console.log('🏝️ Server is listening to 4000 🎧')
-})
+  console.log('Server started on port 4000 🎧');
+});
